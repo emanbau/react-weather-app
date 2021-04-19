a876a0a24ea3f1967c320373ebb9c45d184c756c
Eman Bau <emanbautista410@gmail.com>
2021-04-19 09:07:02 -0400

    Build

---
+++ build/precache-manifest.3f691bfa65e91d1d78b1acfef12d1da8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d32729fecc458a1fb8e5f45b92b44bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf453e1d1de4bf45486",
+    "url": "/static/css/main.7aa188b2.chunk.css"
+  },
+  {
+    "revision": "65fdaaf02eb0a0426ed1",
+    "url": "/static/js/2.f61402b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f61402b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf453e1d1de4bf45486",
+    "url": "/static/js/main.4369cc77.chunk.js"
+  },
+  {
+    "revision": "d7e9bb608f892a3ebf30",
+    "url": "/static/js/runtime-main.2ad375de.js"
+  },
+  {
+    "revision": "ef3bf69d6e48d42e3cc9c9cffe7cda72",
+    "url": "/static/media/imgcold.ef3bf69d.jpg"
+  },
+  {
+    "revision": "b0a63d5b81f8ca1aa70b3c1deca28aea",
+    "url": "/static/media/imgwarm.b0a63d5b.jpg"
+  }
+]);
